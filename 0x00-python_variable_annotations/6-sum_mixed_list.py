e78f912f11281011a08566f350ed23c21c317d62
Uchechukwu Ottah <usottah@gmail.com>
2023-03-31 20:34:30 +0000

    Update: List x Union, not Tuple

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Sums all arguments in a list of integers and floats """
 
-from typing import Tuple
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Tuple[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function def"""
     return float(sum(mxd_lst))
