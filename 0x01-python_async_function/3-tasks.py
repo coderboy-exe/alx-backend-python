c38885690df3e10cbf722b027e0fa8feeb0ef457
Uchechukwu Ottah <usottah@gmail.com>
2023-04-03 21:24:08 +0000

    Completed tasks: wait on random tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -11,5 +11,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """function definition"""
-    #https://superfastpython.com/asyncio-return-value/
+    # https://superfastpython.com/asyncio-return-value/
     return asyncio.create_task(wait_random(max_delay))
