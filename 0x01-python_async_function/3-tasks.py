b4608988900c3b5c5c6c4bea60490fef24effbae
Uchechukwu Ottah <usottah@gmail.com>
2023-04-03 21:17:57 +0000

    Correcte expected return

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> str:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """function definition"""
     #https://superfastpython.com/asyncio-return-value/
     return asyncio.create_task(wait_random(max_delay))
