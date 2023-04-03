b92e668056aff88b18fdcf98457463a3bac91dd7
Uchechukwu Ottah <usottah@gmail.com>
2023-04-03 18:57:37 +0000

    Pycodestyle formatting

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-    an asynchronous coroutine that takes in an integer argument 
+    an asynchronous coroutine that takes in an integer argument
     (max_delay, with a default value of 10) named wait_random that
     waits for a random delay between 0 and max_delay.
 """
@@ -10,6 +10,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-  rand = random.uniform(0, max_delay)
-  await asyncio.sleep(rand)
-  return rand
+    """ Function def"""
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
